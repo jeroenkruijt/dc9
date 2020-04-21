1f28fd4dd3c892fd1050f0b34355a086b885dc81
jeroen kruijt <j.kruijt@st.hanze.nl>
2020-04-21 14:25:10 +0200

    week 1 lesson content

--- js/main.js
+++ js/main.js
@@ -1,16 +1,44 @@
-function preload(){
+var logo;
 
+var cursors;
+
+function preload(){
+    this.load.image("logo", "assets/img.png");
 }
 
 function create(){
+   logo = this.add.image(400, 300, "logo");
 
+   cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update(){
-
+    if(cursors.right.isDown){
+        logo.x++;
+    }
+    if(cursors.left.isDown){
+        logo.x--;
+    }
+    if(cursors.up.isDown){
+        logo.y--;
+    }
+    if(cursors.down.isDown){
+        logo.y++;
+    }
+    if(logo.x > 800){
+        logo.x = 0;
+    }
 }
 
 var config = {
     type: Phaser.AUTO,
-    
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
+
+var game = new Phaser.Game(config);
